daf3aa48c869d4b5cabc8412227b21389c7e8204
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2024-02-21 14:46:26 +0000

    Add CORS settings

--- sitevisor/settings.py
+++ sitevisor/settings.py
@@ -43,10 +43,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'sitevisorapi',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,6 +78,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'sitevisor.wsgi.application'
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
